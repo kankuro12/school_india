2848fcb66e97704f7f4b10c1c7deba3f011fbc7a
chhatraman shrestha <cms111000111@gmail.com>
2022-01-26 17:19:02 +0545

    social links managed

--- resources/views/front/layout/footer.blade.php
+++ resources/views/front/layout/footer.blade.php
@@ -56,15 +56,10 @@
       <div class="row">
         <div class="col-md-6 mb-2">
           <div class="social-links d-flex justify-content-center justify-content-md-start align-items-center">
-            <a href="" class="social-link">
+            <a href="https://www.facebook.com/puschoolofengineering" class="social-link">
               <i class="fab fa-facebook-f"></i>
             </a>
-            <a href="" class="social-link">
-              <i class="fab fa-facebook-f"></i>
-            </a>
-            <a href="" class="social-link">
-              <i class="fab fa-facebook-f"></i>
-            </a>
+          
           </div>
         </div>
         <div class="col-md-6  d-flex justify-content-center justify-content-md-end align-items-center">
